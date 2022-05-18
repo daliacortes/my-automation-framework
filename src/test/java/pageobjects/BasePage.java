782225c70dfcc6d33ed5b393bd2d4446e6ff0e0d
Francisco Mora <fr.morac@duocuc.cl>
2022-05-18 17:12:25 -0400

    Update pom.xml and BasePage.java

--- src/test/java/pageobjects/BasePage.java
+++ src/test/java/pageobjects/BasePage.java
@@ -1,8 +1,24 @@
 package pageobjects;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import steps.Hook;
@@ -19,11 +35,158 @@ public class BasePage {
         PageFactory.initElements(driver, this);
     }
 
-    public void navigateTo(String url){
+    public void navigateTo(String url) {
         driver.get(url);
     }
 
-    public String getPageTitle(){
+    public String getPageTitle() {
         return driver.getTitle();
-    } 
+    }
+
+    public void setText(By locator, String text) {
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+        tab(locator);
+    }
+
+    public void tab(By locator) {
+        driver.findElement(locator).sendKeys(Keys.TAB);
+    }
+
+    public String getText(By locator) {
+        String displayedText = driver.findElement(locator).getText();
+        if (displayedText.isEmpty()) {
+            return driver.findElement(locator).getAttribute("value");
+        } else {
+            return displayedText;
+        }
+    }
+
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public void goBack() {
+        driver.navigate().back();
+    }
+
+    /**
+     * Performs a drag-n-drop operation on a given element by a given x,y offset.
+     *
+     * @param locator The element to be interacted with
+     * @param x       x-coordinate
+     * @param y       Y-coordinate
+     */
+    public void dragAndDropByOffset(By locator, int x, int y) {
+        Actions actions = new Actions(driver);
+        WebElement element = driver.findElement(locator);
+        actions.dragAndDropBy(element, x, y).perform();
+    }
+
+    public void dismissPopup() {
+        driver.switchTo().alert().dismiss();
+    }
+
+    public void acceptPopup() {
+        driver.switchTo().alert().accept();
+    }
+
+    public void setAlertText(String text) {
+        driver.switchTo().alert().sendKeys(text);
+    }
+
+    public void waitForElementText(By locator, String text) {
+        // This is an explicit wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.textToBe(locator, text));
+
+        // This is a FluentWait. It does the same as the above wait, but it is more
+        // customizable
+        // Wait<WebDriver> wait = new FluentWait<>(driver)
+        // .withTimeout(Duration.ofSeconds(3))
+        // .pollingEvery(Duration.ofMillis(250))
+        // .ignoring(NoSuchElementException.class);
+        // wait.until(ExpectedConditions.textToBe(locator, text));
+    }
+
+    public void hoverOverElement(By locator) {
+        WebElement element = driver.findElement(locator);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
+
+    public void scrollElementIntoView(By locator) {
+        WebElement element = driver.findElement(locator);
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    /**
+     * Scrolls the document by the specified number of pixels.
+     *
+     * @param x How many pixels to scroll by, along the x-axis (horizontal).
+     * @param y How many pixels to scroll by, along the y-axis (vertical).
+     */
+    public void scrollPage(int x, int y) {
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("window.scrollBy(" + x + "," + y + ");");
+    }
+
+    /**
+     * Takes screenshot of whole page and uses the current date/time as the file name
+     */
+    public void takeScreenshot() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH-mm-ss-SSS");
+        LocalDateTime dateTime = LocalDateTime.now();
+        takeScreenshot(dateTime.format(formatter));
+    }
+
+    /**
+     * Takes screenshot of whole page and allows you to name the screenshot
+     *
+     * @param screenshotName The screenshot file name
+     */
+    public void takeScreenshot(String screenshotName) {
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("./failed_tests/" + screenshotName + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Takes screenshot of single WebElement
+     */
+    public void takeElementScreenshot(By locator) {
+        WebElement element = driver.findElement(locator);
+        File file = element.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("./screenshot.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param frame The index of the frame to switch to (first frame has index 0)
+     */
+    public void switchFrames(int frame) {
+        driver.switchTo().frame(frame);
+    }
+
+    public void switchToDefaultFrame() {
+        driver.switchTo().defaultContent();
+    }
+
+    public void setCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        driver.manage().addCookie(cookie);
+    }
+
+    public Cookie getCookie(String name) {
+        return driver.manage().getCookieNamed(name);
+    }
 }
